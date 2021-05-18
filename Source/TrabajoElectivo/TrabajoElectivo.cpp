f42958145320e4c2c763dc9bb2d8f70f13b67cd7
LR56c <ronalli56@gmail.com>
2021-05-17 20:57:07 -0400

    Initial commit

---
+++ Source/TrabajoElectivo/TrabajoElectivo.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TrabajoElectivo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TrabajoElectivo, "TrabajoElectivo" );
+ 
